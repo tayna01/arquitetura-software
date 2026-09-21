541bb2ce54a24388ed4efddf324c70b22641015b
Tayna <taynavicente2019@gmail.com>
2026-09-21 16:09:12 -0300

    [Driver: Tayna] [Navigator: Individual] registra clientes no Gateway

--- microservicos/gateway/server.js
+++ microservicos/gateway/server.js
@@ -11,6 +11,9 @@ const PRODUTOS_URL =
 const PEDIDOS_URL =
     process.env.PEDIDOS_URL || "http://localhost:3002";
 
+const CLIENTES_URL =
+    process.env.CLIENTES_URL || "http://localhost:3003";
+
 
 app.use("/produtos", async (req, res) => {
     try {
@@ -52,6 +55,28 @@ app.use("/pedidos", async (req, res) => {
 
         return res.status(503).json({
             erro: "Serviço de Pedidos indisponível"
+        });
+    }
+});
+
+app.use("/clientes", async (req, res) => {
+    try {
+        const resposta = await axios({
+            method: req.method,
+            url: `${CLIENTES_URL}${req.originalUrl}`,
+            data: req.body,
+            params: req.query,
+            timeout: 3000
+        });
+
+        res.status(resposta.status).json(resposta.data);
+    } catch (erro) {
+        if (erro.response) {
+            return res.status(erro.response.status).json(erro.response.data);
+        }
+
+        return res.status(503).json({
+            erro: "Serviço de Clientes indisponível"
         });
     }
 });
